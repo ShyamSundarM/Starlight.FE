b816c4e21dbb30de411b139841996a4edf2dcf6e
Madabathula shyam sundar <m.shyamsundar37@gmail.com>
2025-11-29 19:08:04 +0530

    fixed button loading state

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -5,19 +5,24 @@ export async function VerifyUser(
   email: string,
   password: string
 ): Promise<{ token: string; expiresIn: number } | null> {
-  var resp = await axios.post(
-    `${import.meta.env.VITE_API_BASE_URL}/auth/VerifyUser`,
-    {
-      email,
-      password,
+  try {
+    var resp = await axios.post(
+      `${import.meta.env.VITE_API_BASE_URL}/auth/VerifyUser`,
+      {
+        email,
+        password,
+      }
+    );
+    if (resp.status === 200) {
+      return resp.data;
+    } else if (resp.status === 401) {
+      toast.error("Invalid username/ password");
+      return null;
+    } else {
+      toast.error("Something went wrong. Please try again later.");
+      return null;
     }
-  );
-  if (resp.status === 200) {
-    return resp.data;
-  } else if (resp.status === 401) {
-    toast.error("Invalid username/ password");
-    return null;
-  } else {
+  } catch (error) {
     toast.error("Something went wrong. Please try again later.");
     return null;
   }
